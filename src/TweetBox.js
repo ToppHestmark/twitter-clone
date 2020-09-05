c1d99ff6639bc3ade191044c069fd697140ad371
Suphachai Hestmark <top_hestmark@yahoo.no>
2020-09-05 14:02:10 +0200

    corrected typo in sendTweetImage property

--- src/TweetBox.js
+++ src/TweetBox.js
@@ -28,14 +28,14 @@ function TweetBox() {
     <div className="tweetBox__input">
       <Avatar src="https://avatars3.githubusercontent.com/u/48948074?s=460&u=d8d5a56c477f72be28e39cd0f40b51243180458a&v=4" />
       <input 
-      onChange={e => setTweetMessage(e.target.value)}
+      onChange={(e) => setTweetMessage(e.target.value)}
       value={tweetMessage} 
       placeholder="What's going on?" 
       type="text" />
     </div>
     <input 
+    onChange={(e) => setTweetImage(e.target.value)}
     value={tweetImage}
-    onChange={e => setTweetMessage(e.target.value)}
     className="tweetBox__imageInput" 
     placeholder="Optional: Enter image URL" 
     type="text" 
